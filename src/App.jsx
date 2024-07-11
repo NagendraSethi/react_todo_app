9a77e513f5f4b8d19b7d36c52f44af96c8f5cd33
NagendraSethi <sethi.sethi@kraftheinz.com>
2024-07-11 14:42:00 +0530

    using Context API hooks

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,17 @@ import { useState } from 'react'
 import './App.css'
 import AddTodo from './components/AddTodo/AddTodo'
 import TodoList from './components/TodoList/TodoList'
+import TodoContext from "./context/TodoContext"
 
 function App() {
   const [todos, setTodos]=useState([])
 
-  function addTodos(todoText){
-    let nextId=todos.length + 1;
-    setTodos([...todos,{id:nextId, isFinished: false, text:todoText}])
-  }
   return (
     <>
-      <AddTodo addTodos={addTodos}/>
-      <TodoList setTodos={setTodos} todos={todos}/>
+    <TodoContext.Provider value={{todos,setTodos}}>
+      <AddTodo />
+      <TodoList />
+    </TodoContext.Provider>
     </>
   )
 }
